aa8dd62f0b0cec6461be165376f0570778c6d8dc
Valery91-qw <kryvenka.valery@gmail.com>
2024-05-08 15:42:25 +0300

    fix: rename the CarDAO to the CarDao.

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -1,24 +1,23 @@
 package web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.dao.CarDAO;
+import web.dao.CarDao;
 
 @Controller
 public class CarsController {
 
-    private final CarDAO carDAO;
+    private final CarDao carDao;
 
-    public CarsController(CarDAO carDAO) {
-        this.carDAO = carDAO;
+    public CarsController(CarDao carDAO) {
+        this.carDao = carDAO;
     }
 
     @GetMapping(value = "/cars")
     public String setContent(@RequestParam(value = "count", required = false) Integer count, Model model) {
-        model.addAttribute("cars", carDAO.getCars(count));
+        model.addAttribute("cars", carDao.getCars(count));
         return "cars";
     }
 }
